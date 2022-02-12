b22788c0f026bc3b414b40b04c37fca0a897ce10
premvadee Chadathong <twz.premvadee@gmail.com>
2022-02-12 11:15:33 +0700

    update store

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -46,7 +46,7 @@ class ProductController extends Controller
             'name' => 'store',
             'payload' => $product,
         ];
-        return $result;
+        return response($result, 201);
     }
 
     /**
